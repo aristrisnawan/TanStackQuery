8833696fe7fd803f7dc9622c046da87341e17ebd
aristrisnawan <aristrisnawan35@gmail.com>
2026-03-10 14:25:46 +0700

    Add tan stack query

---
+++ api/todo.ts
@@ -0,0 +1,74 @@
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+export interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+export async function getTodos(): Promise<Todo[]> {
+
+    const response = await fetch(`${API_URL}/todos`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch todos');
+    }
+    const data = await response.json();
+    return data;
+}
+
+export async function addTodo(text: string): Promise<Todo> {
+    const todo = {
+        id: Date.now().toString(),
+        title: text,
+        completed: false
+    }
+
+    const response = await fetch(`${API_URL}/todos`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    if (!response.ok) {
+        throw new Error('Failed to add todo');
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export async function deleteTodo(id: string): Promise<void> {
+    const response = await fetch(`${API_URL}/todos/${id}`, {
+        method: 'DELETE',
+    })
+    if (!response.ok) {
+        throw new Error('Failed to delete todo');
+    }
+    const data = await response.json();
+    return data;
+}
+
+export async function updateTodo(todo: Todo): Promise<Todo> {
+    const response = await fetch(`${API_URL}/todos/`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    if (!response.ok) {
+        throw new Error('Failed to update todo');
+    }
+    const data = await response.json();
+    return data;
+}
+
+export async function getTodoById(id: string): Promise<Todo> {
+    const response = await fetch(`${API_URL}/todos/${id}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch todo');
+    }
+    const data = await response.json();
+    return data;
+}
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,31 +5,35 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const queryClient = new QueryClient
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <QueryClientProvider client={queryClient}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+          headerShown: false,
+          tabBarButton: HapticTab,
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Explore',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          }}
+        />
+      </Tabs>
+    </QueryClientProvider>
   );
 }
